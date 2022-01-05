e6fe994b859a84e1bce3406b3e3d536fdd02aa9e
daironghan <91648878+daironghan@users.noreply.github.com>
2022-01-06 00:06:52 +0800

    Clone player controller
    room manager > player manager > player controller

---
+++ Screencheat/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager Instance;
+    //detect when switch scene and instantiate player manager
+    private void Awake()
+    {
+        if(Instance)//check if another room manager exists
+        {
+            Destroy(gameObject); 
+            return;
+        }
+        DontDestroyOnLoad(gameObject);//only one
+        Instance = this;
+    }
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if(scene.buildIndex == 1)//game scene, instantiate 
+        {
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerManager"), Vector3.zero, Quaternion.identity);//Spawn point
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
