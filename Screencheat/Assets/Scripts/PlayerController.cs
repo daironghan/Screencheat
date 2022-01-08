a62a518112141c445e61eddef83a01bb58e926f4
TCFSH69 <s10060608cces@gmail.com>
2022-01-09 07:19:05 +0800

    final
    FUCK YA

--- Screencheat/Assets/Scripts/PlayerController.cs
+++ Screencheat/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,9 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     public static int score;
-    [SerializeField] private float jumpSpeed = 10f;
+    [SerializeField] private float _jumpForce = 3f;
+    [SerializeField] private float _jumpSpeed = 3.5f;
+    [SerializeField] private float _gravity = 9.81f;
     [SerializeField] private float _speed = 7f;
     [SerializeField] private float _mouseSensitivity = 7f;
     [SerializeField] private float _minCameraView = -70f, _maxCameraView = 80f;
@@ -17,9 +19,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] TMP_Text  txtOver;
     [SerializeField] Button btnMenu;
     Rigidbody rb;
+    MeshRenderer render;
     private CharacterController _charController;
     private Camera _camera;
+    private float timeStamp;
     private float xRotation = 0f;
+    private float _directionY;
     private Vector3 _playerVelocity;
     
 
@@ -27,18 +32,22 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         PV = GetComponent<PhotonView>();
+        render = gameObject.GetComponentInChildren<MeshRenderer>();
     }
     void Start()
     {
+        timeStamp = Time.time;
         score = 0;
         _charController = GetComponent<CharacterController>();
         _camera = Camera.main;
+        render.enabled = false;
 
         if (_charController == null) Debug.Log("No character attached to player");
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
         if (!PV.IsMine)
         {
+            Destroy(transform.GetChild(0).transform.GetChild(1).gameObject);
             GetComponentInChildren<Camera>().rect = new Rect(0, 0, 1, 0.5f);
             Destroy(rb);
             Destroy(GetComponentInChildren<TextMeshProUGUI>());
@@ -52,7 +61,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
        if (!PV.IsMine)
-           return;
+          return;
         PlayerMovement();
         float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity * Time.deltaTime;
@@ -60,7 +69,8 @@ public class PlayerController : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, _minCameraView, _maxCameraView);
         _camera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         transform.Rotate(Vector3.up * mouseX);
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0) && Time.time > timeStamp) {
+            timeStamp = Time.time + 1f;
             shoot();
         }
         
@@ -70,26 +80,27 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector3 movement = transform.forward * vertical + transform.right * horizontal  ;
-        
+        Vector3 movement = transform.forward * vertical + transform.right * horizontal;
+
         if (_charController.isGrounded) {
-            if (Input.GetButton("Jump"))
-                movement.y = jumpSpeed;
+            if (Input.GetButtonDown("Jump")) 
+            {
+                Debug.Log("jump");
+                _directionY = _jumpSpeed;
+            }
+                
         }
-        movement.y -= 9.18f * Time.deltaTime;
+        _directionY -= _gravity * Time.deltaTime;
+        movement.y = _directionY;
+        
         _charController.Move(movement * Time.deltaTime * _speed);
     }
 
     private void FixedUpdate()
     {
         if (transform.position.y < 1.08f) {
-            Debug.Log("00000");
             transform.position = Vector3.right * transform.position.x + Vector3.up * 1.08f + Vector3.forward * transform.position.z;
             _playerVelocity.y = 0f;
-        }
-        else {
-            _playerVelocity.y += -9.18f * Time.deltaTime;
-            _charController.Move(_playerVelocity * Time.deltaTime);
         }
     }
     private void shoot()
@@ -102,7 +113,7 @@ public class PlayerController : MonoBehaviour
                 txtScore.text = "Score: " + score;
             }
         }
-        if(score == 5)
+        if(score == 3)
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
